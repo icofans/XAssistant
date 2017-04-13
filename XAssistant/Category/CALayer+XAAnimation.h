c8784d5a5c99ff94fcb750e304524fedc5e2c860
Qiang <565057208@qq.com>
2017-04-13 14:14:49 -0700

    动画

--- XAssistant/Category/CALayer+XAAnimation.h
+++ XAssistant/Category/CALayer+XAAnimation.h
@@ -8,10 +8,36 @@
 
 #import <QuartzCore/QuartzCore.h>
 
+
 typedef void(^XAAnimationCodeBlock)(void);
 
 @interface CALayer (XAAnimation)
 
+/**
+ 文本抖动了一下
+ */
 - (void)rippleEffect:(XAAnimationCodeBlock)block;
+
+
+/**
+ 添加动画
+
+ @param block 需要动画的代码
+ @param type 类型
+ @param subType 子类型
+ */
+- (void)addAnimationWithType:(NSString *)type
+                    subType:(NSString *)subType
+                  animation:(XAAnimationCodeBlock)block;
+
+
+CA_EXTERN NSString * const XAAnimationCube; //翻转，立方体的翻转效果
+CA_EXTERN NSString * const XAAnimationSuckEffect; //额这个效果就是右下角变小然后整张图移到左上角消失
+CA_EXTERN NSString * const XAAnimationOglFlip; //绕中心翻转
+CA_EXTERN NSString * const XAAnimationRippleEffect; //文本抖动了一下
+CA_EXTERN NSString * const XAAnimationP7ageCurl; //没看出效果
+CA_EXTERN NSString * const XAAnimationPageUnCurl; //翻书的效果
+CA_EXTERN NSString * const XAAnimationCameraIrisHollowClose;//相机关闭
+CA_EXTERN NSString * const XAAnimationCameraIrisHollowOpen; //类似相机照相效果
 
 @end
--- XAssistant/Category/CALayer+XAAnimation.m
+++ XAssistant/Category/CALayer+XAAnimation.m
@@ -8,16 +8,39 @@
 
 #import "CALayer+XAAnimation.h"
 
+
 @implementation CALayer (XAAnimation)
 
-- (void)rippleEffect:(XAAnimationCodeBlock)block
-{
+NSString * const XAAnimationCube= @"cube"; //翻转，立方体的翻转效果
+NSString * const XAAnimationSuckEffect= @"suckEffect"; //额这个效果就是右下角变小然后整张图移到左上角消失
+NSString * const XAAnimationOglFlip= @"oglFlip"; //绕中心翻转
+NSString * const XAAnimationRippleEffect= @"rippleEffect"; //文本抖动了一下
+NSString * const XAAnimationP7ageCurl= @"p7ageCurl"; //没看出效果
+NSString * const XAAnimationPageUnCurl= @"pageUnCurl"; //翻书的效果
+NSString * const XAAnimationCameraIrisHollowClose= @"cameraIrisHollowClose";//相机关闭
+NSString * const XAAnimationCameraIrisHollowOpen= @"cameraIrisHollowOpen"; //类似相机照相效果
+
+/**
+ 文本抖动了一下
+ */
+- (void)rippleEffect:(XAAnimationCodeBlock)block {
     CATransition *animation = [CATransition animation];
     [animation setDuration:0.8];
-    [animation setType:@"rippleEffect"];
+    [animation setType:XAAnimationRippleEffect];
     [animation setSubtype:kCATransitionFromTop];
     block();
     [self addAnimation:animation forKey:nil];
 }
+
+- (void)addAnimationWithType:(NSString *)type subType:(NSString *)subType animation:(XAAnimationCodeBlock)block
+{
+    CATransition *animation = [CATransition animation];
+    [animation setDuration:0.8];
+    [animation setType:type];
+    [animation setSubtype:subType];
+    block();
+    [self addAnimation:animation forKey:nil];
+}
+
 
 @end
